bf6a81d5375809ddf23ffbcf62e159a5e8376448
Luis <lf.mc.almeida@poli.ufrj.br>
2022-09-14 23:01:59 -0300

    add tests to template

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,9 @@ import express,{json} from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import router from './routes/router';
-import dotenv from 'dotenv';
 import errorHandler from './middlewares/errorHandler';
 
 
-
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -19,8 +15,4 @@ app.use(errorHandler);
 
 app.use(router);
 
-const PORT = Number(process.env.PORT) || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}.`);
-});
+export default app
